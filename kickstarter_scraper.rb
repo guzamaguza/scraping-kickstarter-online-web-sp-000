2c085afa9fe5b33155bfab5fa2000136bfa1121d
Mark A Guza <guzamaguza@gmail.com>
2020-02-15 23:48:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,4 +15,6 @@ end
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
+  # location: project.css("u1.project-meta li a span").attribute('class').value
+
 create_project_hash
